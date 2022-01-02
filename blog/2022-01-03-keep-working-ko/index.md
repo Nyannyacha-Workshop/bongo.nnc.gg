0e77b0846b59e63b8f1024365bcfe47d6f72fee2
Nyannyacha <me@barm.co>
2022-01-03 01:08:32 +0900

    doc: initial commit

---
+++ blog/2022-01-03-keep-working-ko/index.md
@@ -0,0 +1,13 @@
+---
+slug: bongo is keep working
+title: Bongo is keep working!
+authors: [nyannyacha]
+tags: [Korean, Language, Minecraft]
+---
+
+Bongo 는 냥냐챠 엔진만을 위해 특별하게 만들어진 마인크래프트 모딩 전용 언어다냥.
+
+![](banner.webp)
+
+이 페이지에 상세한 언어 스펙을 올릴 예정이다냥.<br/>
+준비하고 있으니깐 기달냥
---
+++ docusaurus.config.js
@@ -0,0 +1,112 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Bongo',
+  tagline: 'The Enhanced Language for make easier to modding everything of Minecraft',
+  url: 'https://bongo.nnc.gg',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'Nyannyacha-Workshop', // Usually your GitHub org/user name.
+  projectName: 'bongo_docs', // Usually your repo name.
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: {
+          showReadingTime: true,
+          exclude: ['**/excluded/**'],
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'Bongo',
+        logo: {
+          alt: 'Bongo Mascot',
+          src: 'img/logo.png',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'Tutorial',
+          },
+          {
+            label: 'Blog',
+            to: '/blog',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        logo: {
+          alt: "Nyannyacha Engine",
+          src: 'img/nnc_engine.png'
+        },
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Tutorial',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Discord',
+                href: 'https://nnc.gg/',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/Nyannyacha-Workshop/bongo_docs',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Nyannyacha Workshop.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
